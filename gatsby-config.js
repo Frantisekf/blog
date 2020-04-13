499ae66a6be6dbc01893c3a8d84e0cd19394e4b0
Frantisek Farkas <frantisek@thefemalecompany.com>
2020-04-13 21:54:07 +0200

    config changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Frantisek's blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Frantisek Farkas`,
+      summary: `Dev living in Berlin.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `@FrantisekFarkas`,
     },
   },
   plugins: [
